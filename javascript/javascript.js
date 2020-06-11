2f8de8c9f97da889d51fdcf0a76b984d1764f04f
Encryptar <64878818+Encryptar@users.noreply.github.com>
2020-06-11 12:50:28 +1200

    Changes
    Added some JS functionality

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
-const Applicables [
+const COURSES = [
 	{
 		id: "1",
 		course_title: "Japanese Internship",
@@ -23,16 +23,73 @@ const Applicables [
 		image: "",
 		image_title: ""
 	},
-
-
 ];
 
-function divHider() {
-    var div = document.getElementById('myDIV');
-    if (div.style.display === 'none') {
-        div.style.display = 'block';
-    } else {
-        div.style.display = 'none';
-    }
-};
+function createCourseHTML(course) {
+	// Create an article element to hold all course's content
+	let courseHTML = $("<article>");
 
+	// Create a figure element for displaying the course's image
+	let figure = $("<figure>");
+	let image = $("<img>", {src: course.image, alt: course.image_title});
+	let caption = $("<figcaption>" + course.image_title + "</figcaption>");
+	figure.append(image);
+	figure.append(caption);
+	figure.append(figure);
+	courseHTML.append(figure);
+	// Create a heading element for displaying the course's title
+	let title = $("<h2>" + course.course_title + "</h2>");
+	courseHTML.append(title);
+	// Create a heading element for displaying the course's subtitle
+	let subtitle = $("<h3>" + course.course_subtitle + "</h3>");
+	courseHTML.append(subtitle);
+	// Create a paragraph element for displaying the course's description
+	let descpara = $("<p>" + course.course_description + "</p>")
+	courseHTML.append(descpara)
+	return courseHTML;
+}
+
+function displayCourses(courses) {
+	let coursesList = $("#coursesList");
+
+	// Empty previous courses
+	coursesList.empty();
+	for(let course of courses) {
+		let courseHTML = createCourseHTML(course);
+		coursesList.append(courseHTML);
+	}
+}
+
+// Display a subset of courses when the page loads
+$(document).ready(function(){
+	let mainCourses = [COURSES[0], COURSES[1]];
+
+	displayCourses(mainCourses);
+	$("#search").on("click", search);
+	$("#searchText").on("keyup", search)
+});
+ 
+function search() {
+		// Get the query string
+		let query = $("#searchText").val();
+
+		// Standardize the query (you must do the same when comparing this value against the course_title string)
+		query = query.toLowerCase().trim();
+
+		// Hold the matched queries
+		let matches = [];
+	
+		// Search for courses matching the query string using the course_title property
+		for (var course of COURSES){
+			let courseTitle = course.course_title.toLowerCase();
+			if(courseTitle.includes(query)){
+				matches.push(course)
+			}
+		}
+		
+		
+		// Add each matched course to the matches array
+		
+		// Display all matched courses
+		displayCourses(matches);
+}
